66625f6c005278e8d1380c323ba0725d6a01c272
AWKrol <92876406+AWKrol@users.noreply.github.com>
2022-01-17 12:17:14 +0300

    commit 3 Добавил класс TestCommit3

---
+++ src/test/java/trainingTest/TestCommit3.java
@@ -0,0 +1,12 @@
+package trainingTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCommit3 {
+
+    @Test
+    public void checkCalc() {
+        Assert.assertEquals((4+2), 6);
+    }
+}
